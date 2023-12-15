d161355858c715e73ee5c96ea8f01158f2b10dac
TayLynn Harman <114189908+taylynnharman@users.noreply.github.com>
2023-12-14 17:54:34 -0700

    Delete final directory

--- final/scripts/getDates.js
+++
@@ -1,6 +0,0 @@
-const currentDate = new Date();
-const currentYear = currentDate.getFullYear();
-document.querySelector("#currentYear").textContent = currentYear;
-let lastModif = new Date(document.lastModified);
-lastModified = `Last Modification:  ${lastModif}`;
-document.querySelector("#lastModified").textContent = lastModified;
--- final/scripts/menu.js
+++
@@ -1,8 +0,0 @@
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
-
-hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('open');
-    hamButton.classList.toggle('open');
-});
-
